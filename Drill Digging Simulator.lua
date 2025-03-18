a682d324671291bd2d89703b045134fa823af0db
triquqd717 <actlikeuh@gmail.com>
2025-03-18 22:31:53 +0000

    added drill digging sim

---
+++ Drill Digging Simulator.lua
@@ -0,0 +1,36 @@
+local Games = {
+	[2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 1
+	[4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 2
+	[7449423635] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 3
+	[3623096087] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
+	[8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua", -- PS99 World 1
+	[16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua", -- PS99 World 2
+	[17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua", -- PS99 World 3
+	[10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua", -- The Strongest Battlegrounds
+	[18901165922] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Pet%20GO.lua", -- Pet GO
+	[16732694052] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- Fisch
+	[116495829188952] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+	[70876832253163] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+	[125723653259639] = "https://raw.githubusercontent.com/triquqd717/main/refs/heads/main/other/DrillDiggingSimulator.lua", -- Drill Digging Simulator
+}
+
+local gameid = game.PlaceId
+if Games[gameid] then
+	loadstring(game:HttpGet(Games[gameid]))()
+else
+	local Success, Error = pcall(function()
+		local Library = loadstring(
+			game:HttpGet("https://raw.githubusercontent.com/triquqd717/main/refs/heads/main/test/newLib.lua")
+		)()
+		Library:SetNotification({
+			Title = "Error",
+			Content = "Game Not Supported",
+			Delay = 5,
+		})
+	end)
+
+	if not Success then
+		print("Your game is not supported")
+		warn(Error)
+	end
+end
